a94e1071397741113c26aa5399400057761017d3
AngelNastya <mk_angelnastya@mail.ru>
2017-09-15 14:56:10 +0300

    Файлы заменены

--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -1,13 +1,13 @@
 // helloworld.cpp: определяет точку входа для консольного приложения.
 //
-
+#include "stdafx.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello, World";
+	cout << "Hello, User!";
 	return 0;
 }
 
